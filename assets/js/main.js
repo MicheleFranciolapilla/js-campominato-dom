5e262cd5b71933eda04317efc3c66df0d6324ae6
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-04-09 12:15:16 +0900

    Tempo di gioco: ok + riorganizzazione codice

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,11 +61,17 @@ let     game_grid_exists    = false;
 // Variabile booleana indicante lo status dell'ultimo gioco: concluso o ancora in corso
 let     game_on_going       = false;
 // Variabili booleane usate in fase di conclusione gioco, rispettivamente per sconfitta o per vittoria
-let     exploded            = false; 
 let     won                 = false; 
 // Variabile associata alla griglia di gioco
 let     play_ground;
-
+let     time_str            = "";
+let     time_num            = 0;
+let     hours               = 0;
+let     minutes             = 0;
+let     seconds             = 0;   
+let     timer; 
+let     starting_game       = true; 
+let     time_info_element   = document.getElementById("time_info");
 
 // Funzione che modifica, nel file di stile (.css), il valore della variabile indicante il numero di righe (e colonne) nel gioco
 function set_row_nr_css()
@@ -91,9 +97,10 @@ function new_element(tag_name, class_array, value_)
 function reset_game()
 {
     play_ground.remove();
-    toggle_side_bars(side_bars_hide);
-    toggle_select(false);
+    clock_zero();
     game_grid_exists = false;
+    toggle_side_bars(side_bars_hide);
+    nav_menu_toggle(false);
 }
 
 // Funzione richiamata per verificare se l'ultima cella cliccata conduca o meno alla vittoria. In caso affermativo si procede con il relativo messaggio
@@ -103,7 +110,9 @@ function check_if_win()
     {
         // Termina partita
         won = true;
-        show_message(`Complimenti, hai vinto, totalizzando ${score} punti`);
+        clock_done();
+        game_on_going = false;
+        show_message(`Complimenti, hai vinto, totalizzando ${score} punti in ${time_num} secondi di gioco!`);
     }
 }
 
@@ -171,24 +180,16 @@ function show_explosion(boom_gif)
 {
     console.log("esplosione in corso");
     play_ground.classList.add("p_rel");
-    btn_play_toggle(true);
     play_ground.append(boom_gif);
+    game_on_going = false;
 }
 
 // Funzione che pone fine alla visualizzazione della ".gif" di fine gioco per sconfitta
 function hide_explosion(boom_gif)
 {
     boom_gif.remove();
-    btn_play_toggle(false);
     play_ground.classList.remove("p_rel");
     console.log("esplosione finita");
-}
-
-// Funzione che inverte la visibilità delle select nella navbar, di modo che esse siano presenti solo in fase off-game
-function toggle_select(bool)
-{
-    document.getElementById("rows_number_select").disabled = bool;
-    document.getElementById("bombs_number_select").disabled = bool;
 }
 
 // Gruppo di funzioni esclusive del "gioco-classico"
@@ -342,7 +343,7 @@ function btn_play_toggle(bool)
 function remove_event_listener(clicked_item, index)
 {
     console.log("evento in rimozione");
-    clicked_item.removeEventListener("click", function() {handle_click(clicked_item, index)});
+    this.removeEventListener("click", handle_click());
 }
 
 
@@ -392,6 +393,7 @@ function handle_click(clicked_item, index)
             {
                 // E' presente una bomba ed il gioco si conclude con la sconfitta
                 // Animazione esplosione
+                clock_done();
                 clicked_item.classList.add("clicked_cell");
                 clicked_item.firstChild.classList.toggle("d_none");
                 let boom_gif = new_element("img", ["p_abs", "p_center"], "");
@@ -400,7 +402,6 @@ function handle_click(clicked_item, index)
                 boom_gif.setAttribute("width","250%");
                 boom_gif.setAttribute("height","100%");
                 show_explosion(boom_gif);
-                exploded = true;
                 setTimeout(function()
                 {
                     hide_explosion(boom_gif);
@@ -410,41 +411,76 @@ function handle_click(clicked_item, index)
         }
         else
         {
-            // remove_event_listener(clicked_item, index);
-            // this.removeEventListener("click", function() {handle_click(this, index)});
+            // remove_event_listener(this, index);
+            // this.removeEventListener("click", handle_click(this, index));
             console.log("check for event remove");
 
         }
     }
 }
 
+function clock_zero()
+{
+    time_info_element.innerText = `00: 00': 00"`;
+}
+
+function clock_format()
+{
+    let hrs_str = hours.toString();
+    let min_str = minutes.toString();
+    let sec_str = seconds.toString();
+    if (hours < 10)
+    {
+        hrs_str = "0" + hrs_str;
+    }
+    if (minutes < 10)
+    {
+        min_str = "0" + min_str;
+    }
+    if (seconds < 10)
+    {
+        sec_str = "0" + sec_str;
+    }
+    time_str = hrs_str + ": " + min_str + "': " + sec_str + `"`;
+    time_info_element.innerText = time_str;
+    console.log(time_str);
+}
+
+function clock_update()
+{
+    seconds++;
+    if (seconds == 60)
+    {
+        minutes++;
+        seconds = 0;
+        if (minutes == 60)
+        {
+            hours++;
+            minutes = 0;
+        }
+    }
+    clock_format();
+}
+
+function clock_done()
+{
+    clearInterval(timer);
+    time_num = seconds + (minutes * 60) + (hours * 3600);
+    console.log(time_num);
+}
+
+function clock_init()
+{
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
+    time_num = 0;
+    time_str = "";
+    timer = setInterval(function() {clock_update()},1000);
+}
+
 function create_game_grid()
 {
-    // Inizializzazione variabili e griglia di gioco
-    classic_game_array = [];
-    score = 0;
-    exploded = false;
-    won = false;
-    cells_clicked = 0;
-    cells_total = Math.pow(rows_nr, 2);
-    switch (document.getElementById("bombs_number_select").value)
-    {
-        case "bombs_10":
-            bombs_str = bombs_easy;
-            bombs_number = Math.floor(cells_total * 0.1);
-            break;
-        case "bombs_25":
-            bombs_str = bombs_medium;
-            bombs_number = Math.floor(cells_total * 0.25);
-            break;
-        case "bombs_50":
-            bombs_str = bombs_hard;
-            bombs_number = Math.floor(cells_total * 0.5);
-            break;
-    }
-    cells_valid = cells_total - bombs_number;
-    game_grid_exists = true;
-    game_on_going = true;
     play_ground = document.createElement("div");
     play_ground.setAttribute("id", "game_grid");
     play_ground.classList.add("d_flex", "flex_wrap", "flex_main_btw");
@@ -467,7 +503,6 @@ function create_game_grid()
     load_bombs();
     set_classic_data();
     document.querySelector("#main_core").append(play_ground);
-    toggle_select(true);
 }
 
 
@@ -478,57 +513,145 @@ msg_btn.addEventListener("click", function()
     msg_box.classList.add("d_none");
     let page_overlay = document.getElementById("overlay");
     page_overlay.classList.toggle("d_none");
-    if ((exploded) || (won))
+    if (game_on_going)
     {
-        game_on_going = false;
+        if (starting_game)
+        {
+            clock_init();
+            starting_game = false;
+            console.log("Gioco appena iniziato");
+        }
+        else
+        {
+            console.log("Gioco appena interrotto");
+            game_on_going = false;
+            reset_game();
+        }
+    }
+    else
+    {
+        msg_box.classList.remove("p_top_left");
+        msg_box.classList.add("p_center");
+        if (won)
+        {
+            console.log("Hai vinto");
+        }
+        else
+        {
+            console.log("Hai perso");
+        }
         reset_game();
     }
+
+    // if ((exploded) || (won))
+    // {
+    //     game_on_going = false;
+    //     reset_game();
+    // }
 });
 
 function show_message(message)
 {
+    let msg_box = document.getElementById("message_box");
+    if (!game_on_going)
+    {
+        msg_box.classList.remove("p_center");
+        msg_box.classList.add("p_top_left");
+    }
     let page_overlay = document.getElementById("overlay");
     page_overlay.classList.toggle("d_none");
-    let msg_box = document.getElementById("message_box");
     msg_box.classList.remove("d_none");
     msg_box.classList.add("d_flex", "flex_column", "flex_cross_center");
     msg_box.firstElementChild.innerHTML = `<h2>${message}</h2>`;
 }
 
+function nav_menu_toggle(bool)
+{
+    let button_play = document.querySelector("#btn_play");
+    document.getElementById("rows_number_select").disabled = bool;
+    document.getElementById("bombs_number_select").disabled = bool;
+    button_play.disabled = bool;
+    button_play.classList.toggle("active_btn");
+}
+
 function go_to_game()
 {
-    if (!game_grid_exists)
+    nav_menu_toggle(true);
+    switch (document.getElementById("rows_number_select").value)
     {
-        // Significa che la griglia non c'e' e che non si sta giocando, quindi si puo' iniziare
-        switch (document.getElementById("rows_number_select").value)
-        {
-            case "r_10":
-                rows_nr = rows_10;
-                break;
-            case "r_9":
-                rows_nr = rows_9;
-                break;
-            case "r_7":
-                rows_nr = rows_7;
-                break;
-        }
-        show_message(`Ok, puoi iniziare una nuova partita con ${rows_nr} righe e ${rows_nr} colonne`);
-        set_row_nr_css();
-        create_game_grid();
+        case "r_10":
+            rows_nr = rows_10;
+            break;
+        case "r_9":
+            rows_nr = rows_9;
+            break;
+        case "r_7":
+            rows_nr = rows_7;
+            break;
+    } 
+    set_row_nr_css();
+    cells_total = Math.pow(rows_nr, 2);
+    cells_clicked = 0;
+    score = 0;
+    classic_game_array = [];
+    won = false;
+    switch (document.getElementById("bombs_number_select").value)
+    {
+        case "bombs_10":
+            bombs_str = bombs_easy;
+            bombs_number = Math.floor(cells_total * 0.1);
+            break;
+        case "bombs_25":
+            bombs_str = bombs_medium;
+            bombs_number = Math.floor(cells_total * 0.25);
+            break;
+        case "bombs_50":
+            bombs_str = bombs_hard;
+            bombs_number = Math.floor(cells_total * 0.5);
+            break;
     }
-    else 
-        if (!game_on_going)
-        {
-            show_message("Ok, ricominciamo");
-            reset_game();
-            go_to_game();
-        }
-        else
-        {
-            show_message(`La partita è ancora in corso. <br> Premi sull'icona (${stop_fa_icon}) per terminarla.`);
-            // Significa che e' in corso un gioco e che non e' ancora terminato
-        }
+    cells_valid = cells_total - bombs_number;
+    create_game_grid();
+    game_grid_exists = true;
+    game_on_going = true;
+    starting_game = true;
+    show_message(`Ok, puoi iniziare una nuova partita con ${rows_nr} righe e ${rows_nr} colonne`);
 }
+
+// function go_to_game()
+// {
+//     if (!game_grid_exists)
+//     {
+//         // Significa che la griglia non c'e' e che non si sta giocando, quindi si puo' iniziare
+//         switch (document.getElementById("rows_number_select").value)
+//         {
+//             case "r_10":
+//                 rows_nr = rows_10;
+//                 break;
+//             case "r_9":
+//                 rows_nr = rows_9;
+//                 break;
+//             case "r_7":
+//                 rows_nr = rows_7;
+//                 break;
+//         }
+//         show_message(`Ok, puoi iniziare una nuova partita con ${rows_nr} righe e ${rows_nr} colonne`);
+//         set_row_nr_css();
+//         create_game_grid();
+//     }
+//     else 
+//         if (!game_on_going)
+//         {
+//             show_message("Ok, ricominciamo");
+//             reset_game();
+//             go_to_game();
+//         }
+//         else
+//         {
+//             show_message(`La partita è ancora in corso. <br> Premi sull'icona (${stop_fa_icon}) per terminarla.`);
+//             // Significa che e' in corso un gioco e che non e' ancora terminato
+//         }
+// }
 
 
 
@@ -561,8 +684,10 @@ help_btn.addEventListener("mouseup",function()
 
 stop_btn.addEventListener("click", function()
 {
+  clock_done();
   show_message("La partita è stata chiusa.");  
-  game_on_going = false;
-  reset_game();
+//   game_on_going = false;
+//   reset_game();
 });
 
+clock_zero();
